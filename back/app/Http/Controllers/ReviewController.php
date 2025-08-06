a82ba52c819be91b5fafa34e0ecd28e82432eaf1
sakurai <kanato25kdgn@gmail.com>
2025-08-06 18:09:02 +0900

    レビューの一覧機能の追加

--- back/app/Http/Controllers/ReviewController.php
+++ back/app/Http/Controllers/ReviewController.php
@@ -3,8 +3,27 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Post;
+use App\Models\Dish_image;
+use App\Models\Review;
 
 class ReviewController extends Controller
 {
-    //
+    public function index($id)
+    {
+        $posts = Post::where('dish_id', $id)->get();
+        $name = $posts->name;
+        $images = Dish_image::where('dish_id', $id)->get()->pluck('image_url');
+        $score = [];
+
+        foreach ($posts as $post) {
+            $reviews = Review::where('post_id', $post->id)->get();
+            array_push($score, [$reviews->name, $reviews->score])
+        }
+        return response()->json(
+            'name' => $name,
+            'image_url' => $images,
+            "name_score" => $score
+        )
+    }
 }
