ef7fdd8934eb7046a9af00b730196d5e05041816
sakurai <kanato25kdgn@gmail.com>
2025-08-06 11:27:34 +0900

    イベント表示機能の追加

--- back/app/Http/Controllers/EventController.php
+++ back/app/Http/Controllers/EventController.php
@@ -6,5 +6,10 @@ use Illuminate\Http\Request;
 
 class EventController extends Controller
 {
-    //
+    public function index()
+    {
+        $events = Event::where('is_active', true)
+
+        return response()->json($events);
+    }
 }
