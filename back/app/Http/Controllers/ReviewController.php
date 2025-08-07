dca4a4b1213e3f96812fc72240c24ddd7d6601d5
sakurai <kanato25kdgn@gmail.com>
2025-08-07 12:57:29 +0900

    コード書き直しました

--- back/app/Http/Controllers/ReviewController.php
+++ back/app/Http/Controllers/ReviewController.php
@@ -6,24 +6,32 @@ use Illuminate\Http\Request;
 use App\Models\Post;
 use App\Models\Dish_image;
 use App\Models\Review;
+use App\Models\Dish;
 
 class ReviewController extends Controller
 {
     public function index($id)
     {
-        $posts = Post::where('dish_id', $id)->get();
-        $name = $posts->name;
-        $images = Dish_image::where('dish_id', $id)->get()->pluck('image_url');
+        $posts = Post::where('dish_id', $id)->with('review')->get();
+        $name = Dish::find($id)->name ?? null;
+        $images = Dish_image::where('dish_id', $id)->pluck('image_url');
         $score = [];
-
         foreach ($posts as $post) {
-            $reviews = Review::where('post_id', $post->id)->get();
-            array_push($score, [$reviews->name, $reviews->score])
+            $review = $post->review;
+            if ($review) {
+                $score[] = [
+                    'name' => $review->name,
+                    'score' => $review->score,
+                ];
+            }
         }
+
+        
         return response()->json(
+            [
             'name' => $name,
             'image_url' => $images,
             "name_score" => $score
-        )
+            ]);
     }
 }
