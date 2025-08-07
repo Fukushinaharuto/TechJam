46c7114e38e3d4f183e3b7c036280474b06c375b
sakurai <kanato25kdgn@gmail.com>
2025-08-07 13:43:40 +0900

    お気に入り追加機能作成

--- back/app/Http/Controllers/FavoriteController.php
+++ back/app/Http/Controllers/FavoriteController.php
@@ -6,5 +6,27 @@ use Illuminate\Http\Request;
 
 class FavoriteController extends Controller
 {
-    //
+    public function store($request)
+    {
+        DB::beginTransaction();
+        try {
+            Faborite::create([
+                'user_id' => $request->user_id,
+                'post_id' => $request->dish_id
+            ])
+        } catch (\Exception $e) {
+            DB::rollBack();
+            return response()->json({
+                [
+                    "success" => false,
+                    "message" => "追加に失敗しました"
+                ]
+            });
+        }
+        return response()->json([
+            "success" => true,
+            "message" => "追加に成功しました"
+        ]);
+        DB::commit();
+    }
 }
