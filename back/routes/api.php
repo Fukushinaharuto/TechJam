e9cfa12b35776c4826885727f941d394aed18e6c
sakurai <kanato25kdgn@gmail.com>
2025-08-08 17:17:28 +0900

    ルーティングの実装とミスの修正

--- back/routes/api.php
+++ back/routes/api.php
@@ -5,7 +5,9 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\DishController;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\FavoriteController;
 
+Route::get('/favorite', [FavoriteController::class, 'index']);
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/dish', [DishController::class, 'store']);
