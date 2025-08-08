10ea21779dc18e526e75996dcdce5fbb2d7bfeab
sakurai <kanato25kdgn@gmail.com>
2025-08-08 17:15:06 +0900

    お気に入り一覧機能の追加

--- back/app/Http/Controllers/FavoriteController.php
+++ back/app/Http/Controllers/FavoriteController.php
@@ -6,5 +6,12 @@ use Illuminate\Http\Request;
 
 class FavoriteController extends Controller
 {
-    //
+    public function index(){
+        $favoraite_dish = []
+        $dish_id = Auth::favorite()->dish_id ?? [];
+        foreach ($dish_id as $id){
+            $faboraite_dish[] = Dish::find($id);
+        }
+        return response()->json($favoraite_dish);
+    }
 }
